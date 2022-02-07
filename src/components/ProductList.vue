50c1c64460050138ae7a1e8457884456b011628f
BSevault <b.sevault@gmail.com>
2022-02-07 15:02:58 +0100

    initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>Liste des produits</h1>
+    <div class="product-list">
+            <div
+                v-for="product of products"
+                :key="product.id"
+                :id="'product-' + product.id"
+            >
+                <h2>{{ product.title }}</h2>
+            </div>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    name: "ProductList",
+    setup() {
+        const products = ref(null);
+
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => (products.value = json));
+        console.log(products);
+        return { products };
+    },
+};
+</script>
+
+<style scoped></style>
