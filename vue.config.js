bdd00a6e345255cd3743098e555ed0a4564c2535
BSevault <b.sevault@gmail.com>
2022-02-08 18:52:59 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue.js-product-list/'
-      : '/',
-  }
+    publicPath: '/vue.js-product-list',
+    runtimeCompiler: true
+}
