1091f4054eb4cfe57cd9215af18ad4983e893d57
BSevault <b.sevault@gmail.com>
2022-02-08 15:23:17 +0100

    creating SortingSelect.vue component

---
+++ src/components/SortingSelect.vue
@@ -0,0 +1,20 @@
+<template>
+<label for="sorting">Sort by: &nbsp;</label>
+  <select @change="$emit('sortingOption', $event.target.value)" name="sorting" id="sorting">
+      <option >--</option>
+      <option value="priceAsc">Price: Low to High</option>
+      <option value="priceDesc">Price: High to Low</option>
+      <option value="ratings">Best ratings</option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'SortingSelect',
+    emits: ['sortingOption'],
+}
+</script>
+
+<style scoped>
+
+</style>
